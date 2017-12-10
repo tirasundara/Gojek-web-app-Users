2ad51c5c286ca7c79ca8b922bb6ffd0138d4ee18
Tira Sundara <sundaralinus@gmail.com>
2017-12-10 22:08:24 +0700

    Add the about page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,7 +46,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   # FactoryBot methods
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
