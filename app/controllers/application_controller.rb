08aac26972ecbfc99b9d0d331bbcfbd2176936c9
Tira Sundara <sundaralinus@gmail.com>
2017-12-10 21:18:53 +0700

    Made hello to the world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello, world. This site is under development."
+  end
 end
