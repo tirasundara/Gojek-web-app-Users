156e663d76acb73f1dc8abc0c8d7a62f0bc5f967
Tira Sundara <sundaralinus@gmail.com>
2017-12-11 11:41:46 +0700

    Updated the Title page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title='')
-    base_title = 'RoR Sample App'
+    base_title = 'Go-JEK Web App'
     if page_title.empty?
       base_title
     else
